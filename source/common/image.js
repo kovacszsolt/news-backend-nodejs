c26c26c3d7cb6f6f9acc87cc7980ead55eb97ed6
Kovács Zsolt <smith.zsolt@gmail.com>
2018-12-13 19:27:52 +0100

    WIP

--- source/common/image.js
+++ source/common/image.js
@@ -1,6 +1,44 @@
 const sharp = require('sharp');
 const fsextra = require('fs-extra');
 const util = require('./util');
+const gm = require('gm').subClass({imageMagick: true});
+const resize = (sourceFile, destionationFile, width) => {
+    const targetArray = destionationFile.split('/');
+    fsextra.ensureDirSync(targetArray.slice(0, targetArray.length - 1).join('/'));
+    return new Promise((resolve, reject) => {
+        if (util.getFileExtension(destionationFile) === 'jpg') {
+
+            gm(sourceFile)
+                .resize(width)
+                .noProfile()
+                .write(destionationFile, function (err) {
+                    if (!err) {
+                        resolve(info);
+                    } else {
+                        reject(err);
+                    }
+                });
+            /*
+            sharp(sourceFile)
+                .rotate()
+                .resize(width)
+                .toFile(destionationFile, (err, info) => {
+                    if (err !== null) {
+                        reject(err);
+                    } else {
+                        resolve(info);
+                    }
+                });
+                */
+        } else {
+            fsextra.copySync(sourceFile, destionationFile);
+            resolve(true);
+        }
+    });
+}
+
+
+/*
 const resize = (sourceFile, destionationFile, width) => {
     const targetArray = destionationFile.split('/');
     fsextra.ensureDirSync(targetArray.slice(0, targetArray.length - 1).join('/'));
@@ -23,7 +61,7 @@ const resize = (sourceFile, destionationFile, width) => {
         }
     });
 }
-
+*/
 
 module.exports = {
     resize
