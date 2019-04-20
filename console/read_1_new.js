a6102fd2e3de4e00e6587d12c2e332d04ecac989
Kovács Zsolt <smith.zsolt@gmail.com>
2019-04-20 22:58:32 +0200

    add status to import counter

--- console/read_1_new.js
+++ console/read_1_new.js
@@ -15,6 +15,7 @@ const mongoClient = new MongoClient(config.mongo_server, {useNewUrlParser: true}
 mongoClient.connect(function (err, client) {
     const db = client.db(config.mongo_database);
     const tweetCollection = db.collection('tweet');
+    const statusCollection = db.collection('status');
 
     TwitterClient.get('statuses/user_timeline', {
         screen_name: config.twitter_screen_name,
@@ -27,38 +28,37 @@ mongoClient.connect(function (err, client) {
             const databaseids = tweetList.map(record => record.id);
             const newIds = util.arrayDiff(tweetids, databaseids);
             const newTweets = tweets.filter(tweet => newIds.includes(tweet.id));
+            console.log('newTweets', newTweets.length);
             if (newTweets.length !== 0) {
-                newTweets.forEach((record) => {
-                    records.push({
-                        id: record.id,
-                        text: record.text,
-                        url_short: util.getUrlFromText(record.text),
-                        tags: util.getHastagsFromText(record.text),
-                        created_at: record.created_at,
-                        status: 1
-                    });
-                });
-                tweetCollection.insertMany(
-                    records
-                    , function (err, result) {
-                        if (err === null) {
-                            console.log(result.insertedIds);
-                            if (!fs.existsSync('./_public')) {
-                                fs.mkdirSync('./_public');
-                            }
-                            fs.writeJson('./_public/update.json', {lastAddDate: Math.round(new Date().getTime() / 1000)})
-                                .then(() => {
-                                    process.exit(0);
-                                })
-                                .catch(err => {
-                                    console.error(err)
-                                });
 
-                        } else {
-                            console.log(err);
-                            process.exit(0);
-                        }
-                    });
+                statusCollection.find({}).sort({position: -1}).toArray((err, statusList) => {
+
+                    if (statusList.length === 0) {
+                        statusCollection.insertOne({position: 1, createdate: new Date()}).then((qqq) => {
+                            const newPosition = 1;
+                            insertNewRecords(
+                                newTweets,
+                                tweetCollection,
+                                records,
+                                newPosition
+                            );
+                        });
+                    } else {
+                        console.log('statusList', statusList);
+                        const newPosition = statusList[0].position + 1;
+                        statusCollection.insertOne({
+                            position: newPosition,
+                            createdate: new Date()
+                        }).then(() => {
+                            insertNewRecords(
+                                newTweets,
+                                tweetCollection,
+                                records,
+                                newPosition
+                            );
+                        });
+                    }
+                });
             } else {
                 console.log(newTweets);
                 process.exit(0);
@@ -68,3 +68,38 @@ mongoClient.connect(function (err, client) {
 
     });
 });
+
+insertNewRecords = (newTweets, tweetCollection, records, newPosition) => {
+    newTweets.forEach((record) => {
+        records.push({
+            id: record.id,
+            text: record.text,
+            url_short: util.getUrlFromText(record.text),
+            tags: util.getHastagsFromText(record.text),
+            created_at: record.created_at,
+            position: newPosition,
+            status: 1
+        });
+    });
+    tweetCollection.insertMany(
+        records
+        , function (err, result) {
+            if (err === null) {
+                console.log(result.insertedIds);
+                if (!fs.existsSync('./_public')) {
+                    fs.mkdirSync('./_public');
+                }
+                fs.writeJson('./_public/update.json', {lastAddDate: Math.round(new Date().getTime() / 1000)})
+                    .then(() => {
+                        process.exit(0);
+                    })
+                    .catch(err => {
+                        console.error(err)
+                    });
+
+            } else {
+                console.log(err);
+                process.exit(0);
+            }
+        });
+}
