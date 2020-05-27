30a108e3b5c254fbf9e3d245ef549cf443b9aae2
Kovács Zsolt <smith.zsolt@gmail.com>
2020-05-27 09:37:02 +0200

    alma

--- console/read_meta_from_url.js
+++
@@ -1,7 +0,0 @@
-const meta = require('../common/meta');
-const url = process.argv[2];
-meta.getMetaFromUrl(url, []).then((a) => {
-    console.log('a',a);
-}).catch((e) => {
-    console.log(e);
-});
